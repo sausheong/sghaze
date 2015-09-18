96d6bd341b99cbbfded6250bc87decc11ceb55fd
Sau Sheong Chang <sausheong@gmail.com>
2015-09-19 00:13:52 +0800

    Code cleanup

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
+// Copyright (c) 2015 Chang Sau Sheong
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 var ajax = require('ajax');
 var UI = require('ui');
 var Vector2 = require('vector2');
@@ -9,6 +29,7 @@ var main = new UI.Window({
 	fullscreen: true,
 });
 
+// white background -- I have a white Pebble :)
 var rect = new UI.Rect({
 	position: new Vector2(0, 0),
 	size: new Vector2(144, 168),
@@ -16,6 +37,7 @@ var rect = new UI.Rect({
 });
 main.add(rect);
 
+// main time
 var time = new UI.TimeText({
 	position: new Vector2(0, 52),
 	size: new Vector2(144, 30),
@@ -26,6 +48,7 @@ var time = new UI.TimeText({
 });
 main.add(time);
 
+// day, date, month
 var date = new UI.TimeText({
 	position: new Vector2(0, 90),
 	size: new Vector2(144, 30),
@@ -36,6 +59,7 @@ var date = new UI.TimeText({
 });
 main.add(date);
 
+// overall PSI hourly reading
 var overall = new UI.Text({
 	position: new Vector2(3, 125),
 	size: new Vector2(138, 30),
@@ -45,7 +69,9 @@ var overall = new UI.Text({
 	borderColor: 'black',
 	textAlign: 'center'
 });
+main.add(overall);
 
+// hourly PSI reading for northern Singapore
 var north = new UI.Text({
 	position: new Vector2(4, 0),
 	size: new Vector2(25, 25),
@@ -55,7 +81,9 @@ var north = new UI.Text({
 	backgroundColor: 'black',
 	textAlign: 'center'
 });
+main.add(north);
 
+// hourly PSI reading for western Singapore
 var west = new UI.Text({
 	position: new Vector2(32, 0),
 	size: new Vector2(25, 25),
@@ -65,7 +93,9 @@ var west = new UI.Text({
 	backgroundColor: 'black',
 	textAlign: 'center'
 });
+main.add(west);
 
+// hourly PSI reading for central Singapore
 var center = new UI.Text({
 	position: new Vector2(60, 0),
 	size: new Vector2(25, 25),
@@ -75,7 +105,9 @@ var center = new UI.Text({
 	backgroundColor: 'black',
 	textAlign: 'center'
 });
+main.add(center);
 
+// hourly PSI reading for eastern Singapore
 var east = new UI.Text({
 	position: new Vector2(88, 0),
 	size: new Vector2(25, 25),
@@ -85,7 +117,9 @@ var east = new UI.Text({
 	backgroundColor: 'black',
 	textAlign: 'center'
 });
+main.add(east);
 
+// hourly PSI reading for southern Singapore
 var south = new UI.Text({
 	position: new Vector2(116, 0),
 	size: new Vector2(25, 25),
@@ -95,44 +129,29 @@ var south = new UI.Text({
 	backgroundColor: 'black',
 	textAlign: 'center'
 });
+main.add(south);
 
-ajax({
+// function used to update PSI readings from server
+var update = function() { 
+	ajax({
 		url: 'http://sghazeserv.herokuapp.com/psi/region/all',
 		type: 'json'
 	},
 	function(data) {
-
 		north.text(data.readings.north);
 		west.text(data.readings.west);
 		center.text(data.readings.center);
 		east.text(data.readings.east);
 		south.text(data.readings.south);
-
-		main.add(north);
-		main.add(west);
-		main.add(center);
-		main.add(east);
-		main.add(south);
-
 		overall.text(data.readings.overall + " " + data.descriptors.overall);
-		main.add(overall);
 	});
-
+};
+// update on startup
+update();
 main.show();
 
+// update again on accelerator tapping
 main.on('accelTap', function(e) {
-	ajax({
-			url: 'http://sghazeserv.herokuapp.com/psi/region/all',
-			type: 'json'
-		},
-		function(data) {
-			north.text(data.readings.north);
-			west.text(data.readings.west);
-			center.text(data.readings.center);
-			east.text(data.readings.east);
-			south.text(data.readings.south);
-			overall.text(data.readings.overall + " " + data.descriptors.overall);
-		});
-
+	update();
 	Vibe.vibrate('short');
 });
